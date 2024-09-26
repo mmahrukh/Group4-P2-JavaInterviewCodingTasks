82d721e6461e986a6a790915296d2fb8ad778ac3
mmahrukh <mahrukh.896@gmail.com>
2024-09-26 08:28:33 -0700

    added java coding interview questions

---
+++ src/arrays_codingTasks/JCIQ_32_TripleArray.java
@@ -0,0 +1,18 @@
+package arrays_codingTasks;
+
+public class JCIQ_32_TripleArray {
+    /*
+        Triples Array
+
+        Create a method that will determine if the array has a triple or not.
+        A triple is when an element is repeat 3 times in a row.
+        If any number is repeat 3 times in a row return false.
+        If the array does not have any triples return true
+
+        Ex:
+            noTriples([1, 1, 2, 2, 1]) → true
+            noTriples([1, 1, 2, 2, 2, 1]) → false
+            noTriples([1, 1, 1, 2, 2, 2, 1]) → false
+
+     */
+}
---
+++ src/numbers_codingTasks/JCIQ_09_HappyNumber.java
@@ -0,0 +1,29 @@
+package numbers_codingTasks;
+
+public class JCIQ_09_HappyNumber {
+    /*
+        Happy Number
+
+        Create a method that will check if the given number is a happy or an unhappy number
+
+        A happy number is when the sum of its digits squared eventually result in 1.
+        Hint: Read each digit of the number, square the number and take the number for the next number to be checked
+
+        An unhappy number is when the numbers consistently end up as 4
+
+        Ex:
+            32
+            Happy
+            Breakdown:
+                32 -> 3^2 + 2^2 = 9 + 4 = 13
+                13 -> 1^2 + 3^3 = 1 + 9 = 10
+                10 -> 1^2 + 0^2 = 1 + 0 = 1
+
+        Ex:
+            42
+            Unhappy
+            Breakdown:
+                42 -> 4^2 + 2^2 = 16 + 4 = 20
+                20 -> 2^2 + 0^2 = 4 + 0 = 4
+     */
+}

0e6cf245b960b770281fde5eed55eef490aae1a1
mmahrukh <mahrukh.896@gmail.com>
2024-09-26 10:36:27 -0700

    reverse integer task completed - JCIQ_12

--- src/numbers_codingTasks/JCIQ_12_ReverseString.java
+++ src/numbers_codingTasks/JCIQ_12_ReverseString.java
@@ -1,5 +1,9 @@
 package numbers_codingTasks;
 
+import com.sun.jdi.InvalidTypeException;
+
+import static java.lang.Integer.parseInt;
+
 public class JCIQ_12_ReverseString {
     /*
         Reverse Integer
@@ -16,4 +20,19 @@ public class JCIQ_12_ReverseString {
             input: 1234
             output: 4321
      */
+
+    public static void main(String[] args) {
+        System.out.println("reverseInteger(1234) = " + reverseInteger(1234));
+    }
+
+    public static int reverseInteger(int num) {
+
+        String n = ""+ num;
+        String reverse = "";
+
+        for(int i = n.length()-1; i >= 0; i-- ){
+            reverse += n.charAt(i);
+        }
+        return parseInt(reverse);
+    }
 }

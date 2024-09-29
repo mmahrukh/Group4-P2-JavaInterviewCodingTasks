280d20db3535652d091abfce35f1f4da9461f58b
KostiantynKK <klinchevk@gmail.com>
2024-09-29 16:28:48 -0600

    Created "Kostiantyn" package with the questions

---
+++ src/Kostiantyn /collections_codingTasks/JCIQ_38_RemoveMoreThan100.java
@@ -0,0 +1,18 @@
+package Kostiantyn.collections_codingTasks;
+
+public class JCIQ_38_RemoveMoreThan100 {
+     /*
+        Remove More Than 100
+
+        Create a method that will accept an Integer List and return the List with any value greater than 100 removed
+            parameter: List<Integer>
+            return: List<Integer>
+
+        Ex:
+            input:
+                removeMoreThan100(1000, 50, 24, 200, 124, 29, -255, 981, 102, 87, 99, 1823)
+            output:
+                [50, 24, 29, -255, 87, 99]
+
+     */
+}
---
+++ src/Kostiantyn /numbers_codingTasks/JCIQ_13_SumDigitalUntilOne.java
@@ -0,0 +1,28 @@
+package Kostiantyn.numbers_codingTasks;
+
+public class JCIQ_13_SumDigitalUntilOne {
+     /*
+        Sum All Digits Until One Digit
+
+        Create a method to calculate the sum of all the digits until there is only one digit left.
+        The sum becomes the next number
+            parameter: int
+            return: int
+
+        Ex:
+        input: 45659
+        output:
+                2
+
+        Explanation:
+                4+5+6+5+9=29
+                2+9=11
+                1+1=2
+
+        Test data
+            45659 -> 29 -> 11 -> 2
+            3244 -> 13 -> 4
+            24536 -> 20 -> 2
+
+     */
+}

9554fdc3fb62f34cf6024531d88c2e8e11db4d53
StepanCydeo <grrrabe@gmail.com>
2024-10-01 10:50:40 -0700

    Created Stepan branch and folder, copying task files

---
+++ src/Stepan/collections_codingTasks/JCIQ_37_RemoveAhmed.java
@@ -0,0 +1,18 @@
+package Stepan.collections_codingTasks;
+
+public class JCIQ_37_RemoveAhmed {
+    /*
+        Remove Ahmed
+
+        Create a method that will accept a List of names and return the List with all the Ahmed names removed
+            parameter: List<String>
+            return: List<String>
+
+        Ex:
+            input:
+                removeAhmed("Ahmed", "John", "Eric", "Ahmed")
+            output:
+                [John, Eric]
+
+     */
+}

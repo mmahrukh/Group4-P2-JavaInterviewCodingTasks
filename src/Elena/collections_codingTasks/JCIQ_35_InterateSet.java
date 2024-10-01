da5bf0e61a44b1abfc58e9c1a0fd90b7ac3edf89
elenagaspar <helen.gashpar@gmail.com>
2024-09-30 20:16:53 -0700

    added coding tasks to the package

---
+++ src/Elena/collections_codingTasks/JCIQ_35_InterateSet.java
@@ -0,0 +1,19 @@
+package Elena.collections_codingTasks;
+
+public class JCIQ_35_InterateSet {
+    /*
+        Iterate a Set
+
+        Create a program to iterate through a Set and print the values.
+        The main focus here is to check your understanding of collection types and how Set is different from others
+
+        Ex:
+            input:
+                 set: ["java", "selenium", "sql", "api"]
+            output:
+                java
+                selenium
+                sql
+                api
+     */
+}

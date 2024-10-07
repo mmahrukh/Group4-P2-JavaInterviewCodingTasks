17b981ca8de4ec6514c48ec5dfefa4f856658339
mmahrukh <mahrukh.896@gmail.com>
2024-10-06 19:05:06 -0700

    task 15 completed

--- src/Mahrukh/numbers_codingTasks/JCIQ_15_SwapNumbers.java
+++ src/Mahrukh/numbers_codingTasks/JCIQ_15_SwapNumbers.java
@@ -7,4 +7,25 @@ public class JCIQ_15_SwapNumbers {
         Create a program that will swap two variables' values
             Do it without a third variable
      */
+
+    public static void main(String[] args) {
+        swapNumbers(10, 11);
+    }
+
+
+    public static void swapNumbers(int a, int b){
+
+        System.out.println("Before the swap: ");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After the swap: ");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+    }
 }

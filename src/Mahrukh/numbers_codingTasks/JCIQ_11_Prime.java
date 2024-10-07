eb41af1f7019aaa5c269bebb18aecfe69b1d3cf3
mmahrukh <mahrukh.896@gmail.com>
2024-10-06 20:34:53 -0700

    task 11 - Prime numbers completed

--- src/Mahrukh/numbers_codingTasks/JCIQ_11_Prime.java
+++ src/Mahrukh/numbers_codingTasks/JCIQ_11_Prime.java
@@ -14,4 +14,23 @@ public class JCIQ_11_Prime {
             7 -> prime, because only 1 and 7 can be divided evenly
             6 -> not prime, because 6 is divisible by 2 and 3 also
      */
+    public static void main(String[] args) {
+        System.out.println("isPrime(7) = " + isPrime(7));
+        System.out.println("isPrime(6) = " + isPrime(6));
+    }
+
+    public static boolean isPrime(int n){
+
+        if (n <= 1){
+            return false;                       // number less than or equal to 1 are not prime
+        }
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0){
+                return false;                   // if n is divisible by any number it is not prime
+            }
+        }
+
+        return true;
+    }
 }

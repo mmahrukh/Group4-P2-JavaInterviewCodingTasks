b486e762979ba88f9a4165034e731c493f3ff195
mmahrukh <mahrukh.896@gmail.com>
2024-10-07 22:54:09 -0700

    task 07 - Factorial completed edited

--- src/Mahrukh/numbers_codingTasks/JCIQ_07_FactorialNumber.java
+++ src/Mahrukh/numbers_codingTasks/JCIQ_07_FactorialNumber.java
@@ -25,16 +25,16 @@ public class JCIQ_07_FactorialNumber {
         8 -> 40320
  */
     public static void main(String[] args) {
-        System.out.println(factorial(5)); // Expected output: 120
-        System.out.println(factorial(6)); // Expected output: 720
-        System.out.println(factorial(8)); // Expected output: 40320
+        System.out.println(getFactorial(5)); // Expected output: 120
+        System.out.println(getFactorial(6)); // Expected output: 720
+        System.out.println(getFactorial(8)); // Expected output: 40320
     }
 
-    public static int factorial(int number) {
-        int result = 1;
-        for (int i = 1; i <= number; i++) {
-            result *= i;
+    public static int getFactorial(int num){
+        int factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
         }
-        return result;
+        return factorial;
     }
 }

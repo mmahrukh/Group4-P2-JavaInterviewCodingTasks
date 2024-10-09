6d6e9438a621b3234126fbbaf0d65ba10d46c225
mmahrukh <mahrukh.896@gmail.com>
2024-10-08 21:24:03 -0700

    task 09 - Happy Number completed

--- src/Mahrukh/numbers_codingTasks/JCIQ_09_HappyNumber.java
+++ src/Mahrukh/numbers_codingTasks/JCIQ_09_HappyNumber.java
@@ -26,4 +26,25 @@ public class JCIQ_09_HappyNumber {
                 42 -> 4^2 + 2^2 = 16 + 4 = 20
                 20 -> 2^2 + 0^2 = 4 + 0 = 4
      */
+    public static void main(String[] args) {
+        System.out.println(isHappy(32)); // Expected output: Happy
+        System.out.println(isHappy(42)); // Expected output: Unhappy
+    }
+
+    public static String isHappy(int n){
+
+        while (n != 1 && n != 4){
+
+            int sum = 0;
+
+            while (n > 0){  // to read all the digits
+               int eachDigit = n % 10;
+               sum += Math.pow(eachDigit, 2);       // sum += eachDigit * eachDigit
+               n /= 10;
+            }
+
+            n = sum;
+        }
+        return n == 1? "Happy" : "Unhappy";
+    }
 }

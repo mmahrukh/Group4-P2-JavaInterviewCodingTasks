371cea4c977a2761e552e9acf3424fbb81f9081a
mmahrukh <mahrukh.896@gmail.com>
2024-10-08 21:08:40 -0700

    task 03 - CodilityTestCoders completed

--- src/Mahrukh/numbers_codingTasks/JCIQ_03_ConsecutiveNumbers.java
+++ src/Mahrukh/numbers_codingTasks/JCIQ_03_ConsecutiveNumbers.java
@@ -39,4 +39,38 @@ public class JCIQ_03_ConsecutiveNumbers {
                 TestCoders
                 Codility
      */
+
+    public static void main(String[] args) {
+        consecutiveNumbers(16);
+
+        // Negative Testing:
+        //consecutiveNumbers(-1);
+    }
+
+    public static void consecutiveNumbers(int n){
+
+        if (n < 0){
+            throw new IllegalArgumentException("N should be a positive integer");
+        }
+
+        for(int i = 1; i <= n; i++){
+            if(i % 2 == 0 && i % 3 == 0 && i % 5 == 0){
+                System.out.println("CodilityTestCoders");
+            } else if(i % 2 == 0 && i % 3 == 0){
+                System.out.println("CodilityTest");
+            } else if(i % 2 == 0 && i % 5 == 0){
+                System.out.println("CodilityCoders");
+            } else if(i % 3 == 0 && i % 5 == 0){
+                System.out.println("TestCoders");
+            } else if(i % 2 == 0){
+                System.out.println("Codility");
+            } else if(i % 3 == 0){
+                System.out.println("Test");
+            } else if(i % 5 == 0){
+                System.out.println("Coders");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
 }
